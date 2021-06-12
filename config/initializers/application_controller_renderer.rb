621c4efeddad6a5399611b9f1e68a68a04563caf
whakapapa-HTML <uriah.heep0810@gmail.com>
2021-06-12 10:17:12 +0000

    gitの修正

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '36fd8ae82ad89da93afd310c5121f0a3b162401477518a5440758b9d3d6426ed1bac329ceb353017b0188a1d3e57889c68c8d2ffda99babfcd6a8c3ecf5c7f78'
